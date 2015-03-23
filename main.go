d2453b61fe21088b757d073452287d39c54d508f
Giulio De Donato <liuggio@gmail.com>
2015-03-23 20:58:28 +0100

    main and logger

---
+++ main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func helloHandler(w http.ResponseWriter, r *http.Request) {
+
+	if r.URL.Path == "" || r.URL.Path == "/" {
+		http.Error(w, "Must Contain a Name", http.StatusBadRequest)
+		return
+	}
+
+	fmt.Fprintf(w, "Hello %s.\n", r.URL.Path[1:])
+}
+
+func main() {
+	listenAndServe(helloHandler)
+}
+
+func listenAndServe(handlerFunc http.HandlerFunc) {
+	log.Println("Starting 8080")
+	http.ListenAndServe(":8080", http.HandlerFunc(handlerFunc))
+}
+
+func mainChaining() {
+	listenAndServe(logger(single(helloHandler, "localhost:8080")))
+}
